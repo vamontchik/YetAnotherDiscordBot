fc5b111da3c06a66f78681a2bd5de7bbc207ef9d
Vladimir Montchik <vlad.montchik@gmail.com>
2021-07-18 15:16:26 -0600

    Minor cleanup

--- Program.cs
+++ Program.cs
@@ -6,24 +6,14 @@ using System.Threading.Tasks;
 
 namespace DiscordBot
 {
-    /*
-     * TODO: 
-     * 1) create message parsing class for OnMessageRecieved and OnMessageUpdated 
-     *      - figure out a way to deal with embedded images, etc
-     */          
-
     class Program
     {
         private DiscordSocketClient _client;
 
-        public static void Main(string[] args)
-        {
-            // run the discord bot
-            new Program().MainAsync().GetAwaiter().GetResult();
-        } 
+        public static void Main() => new Program().MainAsync().GetAwaiter().GetResult();
 
         public async Task MainAsync()
-        {
+        { 
             // for local runs: C:\Users\woofers\source\repos\DiscordBot\DiscordBot\token.txt
             var token = File.ReadAllText("token.txt");
 
@@ -35,16 +25,16 @@ namespace DiscordBot
             await _client.LoginAsync(TokenType.Bot, token);
             await _client.StartAsync();
 
-            _client.Ready += OnReady;
             _client.MessageReceived += OnMessageReceived;
-            _client.MessageUpdated += OnMessageUpdated;
+            _client.MessageUpdated += OnMessageUpdatedAsync;
             
             await Task.Delay(-1);
         }
 
-        private async Task OnMessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
-        {
-            // NOTE: If the message was not in the cache, downloading it will result in getting a copy of `after`.
+        private async Task OnMessageUpdatedAsync(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+        {   
+            // NOTE: If the message was not in the cache,
+            //       downloading it will result in getting a copy of `after`.
             var msg = await before.GetOrDownloadAsync();
 
             var afterTimestamp = after.Timestamp;
@@ -63,20 +53,19 @@ namespace DiscordBot
             var channel = arg.Channel;
             var content = arg.Content;
             var timestamp = arg.Timestamp;
+            
             Console.WriteLine($"[{timestamp}]\tAuthor: {author}\tChannel: {channel}");
             Console.WriteLine($"{content}");
 
-            return Task.CompletedTask;
-        }
+            // DO THE THING :D
 
-        private Task OnReady()
-        {
             return Task.CompletedTask;
         }
 
         private Task OnLogMessageEvent(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
+            
             return Task.CompletedTask;
         }
     }

c7cd365f3fc1ba9eb16311248cac69cc7f067c1d
Vlad <vlad.montchik@gmail.com>
2023-07-31 08:40:10 -0600

    file scoped namespaces in StatsCommand.cs

--- src/Command/RockPaperScissors/StatsCommand.cs
+++ src/Command/RockPaperScissors/StatsCommand.cs
@@ -1,32 +1,31 @@
 ﻿using System.Threading.Tasks;
 using Discord.WebSocket;
 
-namespace DiscordBot.Command.RockPaperScissors
+namespace DiscordBot.Command.RockPaperScissors;
+
+internal class StatsEntryPoint
 {
-    internal class StatsEntryPoint
+    private readonly ISocketMessageChannel _destChannel;
+    private readonly ulong _userId;
+    private readonly string _userName;
+
+    public StatsEntryPoint(SocketMessage socketMessage)
     {
-        private readonly ISocketMessageChannel _destChannel;
-        private readonly ulong _userId;
-        private readonly string _userName;
+        _destChannel = socketMessage.Channel;
+        _userId = socketMessage.Author.Id;
+        _userName = socketMessage.Author.Username;
+    }
 
-        public StatsEntryPoint(SocketMessage socketMessage)
+    public async Task DoAsync()
+    {
+        var statsManager = StatsManager.Instance;
+        var stat = statsManager.GetStat(_userId);
+        if (stat is null)
+            await _destChannel.SendMessageAsync($"No stats found for {_userName}");
+        else
         {
-            _destChannel = socketMessage.Channel;
-            _userId = socketMessage.Author.Id;
-            _userName = socketMessage.Author.Username;
-        }
-
-        public async Task DoAsync()
-        {
-            var statsManager = StatsManager.Instance;
-            var stat = statsManager.GetStat(_userId);
-            if (stat is null)
-                await _destChannel.SendMessageAsync($"No stats found for {_userName}");
-            else
-            {
-                var statsStr = stat.ComputeStats();
-                await _destChannel.SendMessageAsync($"{statsStr}");
-            }
+            var statsStr = stat.ComputeStats();
+            await _destChannel.SendMessageAsync($"{statsStr}");
         }
     }
 }

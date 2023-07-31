9c864a62a35611638e3bb0ccdc0b60a924eca827
Vlad <vlad.montchik@gmail.com>
2023-07-31 08:38:41 -0600

    file scoped namespaces in RPSTypeExtensions.cs

--- src/Command/RockPaperScissors/RPSTypeExtensions.cs
+++ src/Command/RockPaperScissors/RPSTypeExtensions.cs
@@ -1,22 +1,21 @@
-﻿namespace DiscordBot.Command.RockPaperScissors
+﻿namespace DiscordBot.Command.RockPaperScissors;
+
+internal static class RpsTypeExtensions
 {
-    internal static class RpsTypeExtensions
+    public static int Compare(this RpsType ours, RpsType theirs)
     {
-        public static int Compare(this RpsType ours, RpsType theirs)
-        {
-            if (ours == RpsType.Rock && theirs == RpsType.Scissors ||
-                ours == RpsType.Paper && theirs == RpsType.Rock ||
-                ours == RpsType.Scissors && theirs == RpsType.Paper
-            )
-                return 1;
+        if (ours == RpsType.Rock && theirs == RpsType.Scissors ||
+            ours == RpsType.Paper && theirs == RpsType.Rock ||
+            ours == RpsType.Scissors && theirs == RpsType.Paper
+           )
+            return 1;
 
-            if (theirs == RpsType.Rock && ours == RpsType.Scissors ||
-                theirs == RpsType.Paper && ours == RpsType.Rock ||
-                theirs == RpsType.Scissors && ours == RpsType.Paper
-            )
-                return -1;
+        if (theirs == RpsType.Rock && ours == RpsType.Scissors ||
+            theirs == RpsType.Paper && ours == RpsType.Rock ||
+            theirs == RpsType.Scissors && ours == RpsType.Paper
+           )
+            return -1;
 
-            return 0;
-        }
+        return 0;
     }
 }

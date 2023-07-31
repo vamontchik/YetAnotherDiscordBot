bc68d9cc42d0a712b380c7b16207b08cb6f80f38
Vlad <vlad.montchik@gmail.com>
2023-07-31 08:33:04 -0600

    use file scoped namespaces, remove #region usage, and run auto-formatter in Gremlin.cs

--- src/Command/Gremlin/Gremlin.cs
+++ src/Command/Gremlin/Gremlin.cs
@@ -1,54 +1,45 @@
 ﻿using System;
 
-namespace DiscordBot.Command.Gremlin
+namespace DiscordBot.Command.Gremlin;
+
+internal class Gremlin
 {
-    internal class Gremlin
+    private Gremlin()
     {
-        #region Singleton
-        
-        private Gremlin()
-        {
-            Attack = 0;
-            Defense = 0;
-            Level = 0;
-        }
+        Attack = 0;
+        Defense = 0;
+        Level = 0;
+    }
 
-        private static readonly Lazy<Gremlin> Lazy = new(() => new Gremlin());
+    private static readonly Lazy<Gremlin> Lazy = new(() => new Gremlin());
 
-        public static Gremlin Instance => Lazy.Value;
+    public static Gremlin Instance => Lazy.Value;
 
-        #endregion
-        
-        #region Auto-Properties
-        
-        private int Attack { get; set; }
+    private int Attack { get; set; }
 
-        private int Defense { get; set; }
-        
-        private int Level { get; set; }
-        
-        #endregion
+    private int Defense { get; set; }
 
-        private static readonly object UpdateLock = new();
+    private int Level { get; set; }
 
-        public void Update()
-        {
-            lock (UpdateLock)
-                UpdateStats();
-        }
+    private static readonly object UpdateLock = new();
 
-        private void UpdateStats()
-        {
-            var rand = new Random();
-            Attack += rand.Next(0, 10);
-            Defense += rand.Next(0, 10);
-            Level += 1;
-        }
+    public void Update()
+    {
+        lock (UpdateLock)
+            UpdateStats();
+    }
 
-        public override string ToString()
-        {
-            lock(UpdateLock)
-                return $"Gremlin Level {Level}: Attack {Attack}, Defense: {Defense}";
-        }
+    private void UpdateStats()
+    {
+        var rand = new Random();
+        Attack += rand.Next(0, 10);
+        Defense += rand.Next(0, 10);
+        Level += 1;
+    }
+
+    public override string ToString()
+    {
+        lock (UpdateLock)
+            return $"Gremlin Level {Level}: Attack {Attack}, Defense: {Defense}";
     }
 }

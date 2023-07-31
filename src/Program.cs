2ffba81be2917435b2596b76d0045f0248df8a79
Vlad <vlad.montchik@gmail.com>
2023-07-31 08:36:54 -0600

    changes in Program.cs:
    - make main method async, and await on `StartupAsync`
    - remove use of #region
    - adjust comments not to reveal local paths
    - adjust comments to use "admin_id.txt"
    - make use of in-lining variables & expression bodies in a few places
    - set GatewayIntents to GatewayIntents.All for the created DiscordSocketClient in CreateDiscordSocketClient. Also, made intent changes on discord dev site, giving bot all intents possible
    - use file-scoped namespace

--- src/Program.cs
+++ src/Program.cs
@@ -4,91 +4,68 @@ using System;
 using System.IO;
 using System.Threading.Tasks;
 
-namespace DiscordBot
+namespace DiscordBot;
+
+internal class Program
 {
-    internal class Program
+    private readonly DiscordSocketClient _client;
+    private readonly string _adminId;
+    private readonly string _token;
+    private const int MessageCacheSize = 100;
+
+    public static async Task Main() => await new Program().StartupAsync();
+
+    private Program()
     {
-        private readonly DiscordSocketClient _client;
-        private readonly string _adminId;
-        private readonly string _token;
-        private const int MessageCacheSize = 100;
-
-        public static void Main() => new Program().StartupAsync().Wait();
-
-        private Program()
-        {
-            _token = ReadTokenFile();
-            _adminId = ReadAdminIdFile();
-            _client = CreateDiscordSocketClient();
-            SubscribeEventHandlers();
-        }
-
-        #region Constructor Helpers        
-
-        private static string ReadTokenFile()
-        {
-            // for local runs:
-            // @"C:\Users\woofers\source\repos\DiscordBot\DiscordBot\token.txt"
-            // for docker builds:
-            // "token.txt"
-
-            return File.ReadAllText("token.txt");
-        }
-
-        private static string ReadAdminIdFile()
-        {
-            // for local runs:
-            // @"C:\Users\woofers\source\repos\DiscordBot\DiscordBot\id.txt"
-            // for docker builds:
-            // "id.txt"
-
-            return File.ReadAllText("id.txt");
-        }
-
-        private static DiscordSocketClient CreateDiscordSocketClient()
-        {
-            var config = new DiscordSocketConfig { MessageCacheSize = MessageCacheSize };
-            return new DiscordSocketClient(config);
-        }
-
-        private void SubscribeEventHandlers()
-        {
-            _client.Log += OnLogMessageEvent;
-            _client.MessageReceived += OnMessageReceived;
-        }
-        
-        #endregion
-
-        private async Task StartupAsync()
-        {
-            await _client.LoginAsync(TokenType.Bot, _token);
-            await _client.StartAsync();
-            await Task.Delay(-1);
-        }
-
-        #region Events
-        
-        private static Task OnLogMessageEvent(LogMessage msg)
-        {
-            Console.WriteLine(msg.ToString());
-            return Task.CompletedTask;
-        }
-
-        private async Task OnMessageReceived(SocketMessage socketMessage)
-        {
-            var msg = new Message(socketMessage, IsAdminMessage(socketMessage));
-            await MessageProcessor.Instance.ProcessMessage(msg);
-        }
-
-        #endregion
-
-        #region Util
-
-        private bool IsAdminMessage(SocketMessage socketMessage)
-        {
-            return _adminId == socketMessage.Id.ToString();
-        }
-
-        #endregion
+        _token = ReadTokenFile();
+        _adminId = ReadAdminIdFile();
+        _client = CreateDiscordSocketClient();
+        SubscribeEventHandlers();
     }
+
+    // for local runs:
+    // @"<full-path-to>\token.txt"
+    // for docker builds:
+    // "token.txt"
+    private static string ReadTokenFile() => File.ReadAllText("token.txt");
+
+    // for local runs:
+    // @"<full-path-to>\admin_id.txt"
+    // for docker builds:
+    // "admin_id.txt"
+    private static string ReadAdminIdFile() => File.ReadAllText("admin_id.txt");
+
+    private static DiscordSocketClient CreateDiscordSocketClient() =>
+        new(new DiscordSocketConfig
+        {
+            MessageCacheSize = MessageCacheSize,
+            GatewayIntents = GatewayIntents.All
+        });
+
+    private void SubscribeEventHandlers()
+    {
+        _client.Log += OnLogMessageEvent;
+        _client.MessageReceived += OnMessageReceived;
+    }
+
+    private async Task StartupAsync()
+    {
+        await _client.LoginAsync(TokenType.Bot, _token);
+        await _client.StartAsync();
+        await Task.Delay(-1);
+    }
+
+    private static Task OnLogMessageEvent(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+
+    private async Task OnMessageReceived(SocketMessage socketMessage)
+    {
+        var msg = new Message(socketMessage, IsAdminMessage(socketMessage));
+        await MessageProcessor.Instance.ProcessMessage(msg);
+    }
+
+    private bool IsAdminMessage(SocketMessage socketMessage) => _adminId == socketMessage.Id.ToString();
 }

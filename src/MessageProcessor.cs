2e33cbf6738471b2def23653de50e5cec29058bc
Vlad <vlad.montchik@gmail.com>
2023-07-31 08:35:00 -0600

    use file-scoped namespaces, use `sealed` keyword, remove #region usage in MessageProcessor.cs

--- src/MessageProcessor.cs
+++ src/MessageProcessor.cs
@@ -7,62 +7,57 @@ using DiscordBot.Command.Empty;
 using DiscordBot.Command.Gremlin;
 using DiscordBot.Command.RockPaperScissors;
 
-namespace DiscordBot
+namespace DiscordBot;
+
+internal sealed class MessageProcessor
 {
-    internal class MessageProcessor
+    private MessageProcessor()
     {
-        #region Singleton
-
-        private MessageProcessor()
+        _commands = new HashSet<string>
         {
-            _commands = new HashSet<string>
-            {
-                "!rps",
-                "!gremlin"
-            };
-        }
-        
-        private static readonly Lazy<MessageProcessor> Lazy = new(() => new MessageProcessor());
+            "!rps",
+            "!gremlin"
+        };
+    }
 
-        public static MessageProcessor Instance => Lazy.Value;
+    private static readonly Lazy<MessageProcessor> Lazy = new(() => new MessageProcessor());
 
-        #endregion
+    public static MessageProcessor Instance => Lazy.Value;
 
-        private readonly HashSet<string> _commands;
+    private readonly HashSet<string> _commands;
 
-        public async Task ProcessMessage(Message message)
+    public async Task ProcessMessage(Message message)
+    {
+        // TODO: when to implement use of message.IsAdmin ?
+        var command = ParseMessage(message.SocketMessage);
+        await command.ExecuteAsync();
+    }
+
+    private ICommand ParseMessage(SocketMessage socketMessage)
+    {
+        var contents = socketMessage.Content;
+        var split = contents.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+        return IsValidCommand(split) ? ParseIntoCommand(split, socketMessage) : new EmptyCommand();
+    }
+
+    private bool IsValidCommand(IReadOnlyList<string> splitMessageContents)
+    {
+        if (splitMessageContents.Count == 0)
+            return false;
+
+        var baseCommand = splitMessageContents[0];
+        return _commands.Contains(baseCommand);
+    }
+
+    private static ICommand ParseIntoCommand(
+        IReadOnlyList<string> splitMessageContents, SocketMessage socketMessage)
+    {
+        var baseCommand = splitMessageContents[0];
+        return baseCommand switch
         {
-            // TODO: when to implement use of message.IsAdmin ?
-            var command = ParseMessage(message.SocketMessage);
-            await command.ExecuteAsync();
-        }
-
-        private ICommand ParseMessage(SocketMessage socketMessage)
-        {
-            var contents = socketMessage.Content;
-            var split = contents.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-            return IsValidCommand(split) ? ParseIntoCommand(split, socketMessage) : new EmptyCommand();
-        }
-
-        private bool IsValidCommand(IReadOnlyList<string> splitMessageContents)
-        {
-            if (splitMessageContents.Count == 0)
-                return false;
-
-            var baseCommand = splitMessageContents[0];
-            return _commands.Contains(baseCommand);
-        }
-
-        private static ICommand ParseIntoCommand(
-            IReadOnlyList<string> splitMessageContents, SocketMessage socketMessage)
-        {
-            var baseCommand = splitMessageContents[0];
-            return baseCommand switch
-            {
-                "!rps" => new RockPaperScissorsCommand(splitMessageContents, socketMessage),
-                "!gremlin" => new GremlinCommand(socketMessage),
-                _ => new EmptyCommand()
-            };
-        }
+            "!rps" => new RockPaperScissorsCommand(splitMessageContents, socketMessage),
+            "!gremlin" => new GremlinCommand(socketMessage),
+            _ => new EmptyCommand()
+        };
     }
 }

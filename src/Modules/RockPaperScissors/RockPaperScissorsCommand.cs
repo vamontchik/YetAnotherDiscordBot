71fffd92c92ae276916e2cb03af848ad3eca3180
Vlad <vlad.montchik@gmail.com>
2023-08-04 12:32:53 -0600

    add adjusted command class, making use of SocketCommandContext and DI

---
+++ src/Modules/RockPaperScissors/RockPaperScissorsCommand.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Modules.RockPaperScissors;
+
+public sealed class RockPaperScissorsCommand
+{
+    private readonly Random _rnd;
+    private readonly Dictionary<int, RpsType> _choices;
+    private readonly Dictionary<string, RpsType> _strChoices;
+    private readonly string _arg;
+    private readonly bool _isStatCheck;
+    private readonly StatsManager _statsManager;
+    private readonly SocketCommandContext _socketCommandContext;
+
+    public RockPaperScissorsCommand(
+        string argument, 
+        SocketCommandContext socketCommandContext,
+        StatsManager statsManager)
+    {
+        _rnd = new Random();
+        _choices = CreateChoicesDictionary();
+        _strChoices = CreateStrChoicesDictionary();
+
+        _arg = argument;
+        _isStatCheck = _arg.Equals("stats");
+        
+        _statsManager = statsManager;
+
+        _socketCommandContext = socketCommandContext;
+    }
+
+    private static Dictionary<int, RpsType> CreateChoicesDictionary() => new()
+    {
+        { 0, RpsType.Rock },
+        { 1, RpsType.Paper },
+        { 2, RpsType.Scissors }
+    };
+
+    private static Dictionary<string, RpsType> CreateStrChoicesDictionary() => new()
+    {
+        { "rock", RpsType.Rock },
+        { "paper", RpsType.Paper },
+        { "scissors", RpsType.Scissors }
+    };
+
+    public async Task ExecuteAsync()
+    {
+        if (_isStatCheck)
+            await new StatsEntryPoint(_socketCommandContext, _statsManager).DoAsync();
+        else if (!ValidUserChoice())
+            await SendErrorMessageAsync();
+        else
+            await DoGameAsync();
+    }
+
+    private bool ValidUserChoice() => _strChoices.ContainsKey(_arg);
+
+    private async Task SendErrorMessageAsync() =>
+        await _socketCommandContext.Message.ReplyAsync("That's not a rock, paper, or scissors!");
+
+    private async Task DoGameAsync()
+    {
+        var bot = CreateBotPlayer();
+        var player = CreateUserPlayer();
+        var gameResult = GetGameResult(bot, player);
+        await SendGameResultAsync(gameResult, bot, player);
+        _statsManager.Update(gameResult);
+    }
+
+    private async Task SendGameResultAsync(GameResult gameResult, RpsPlayer bot, RpsPlayer user)
+    {
+        var winner = gameResult.GetWinner();
+        var resultStr = winner is not null ? $"Result: {winner.Name} Wins!" : "Result: Tie";
+        await _socketCommandContext.Message.ReplyAsync($"User: {user.Type}, Bot: {bot.Type}, {resultStr}");
+    }
+
+    private RpsPlayer CreateBotPlayer()
+    {
+        var botChoice = CreateRandomBotChoice();
+        var botChoiceAsType = _choices[botChoice];
+        return new RpsPlayer(botChoiceAsType, "Bot", RpsPlayer.BotId);
+    }
+
+    private int CreateRandomBotChoice() => _rnd.Next(Enum.GetNames(typeof(RpsType)).Length);
+
+    private RpsPlayer CreateUserPlayer()
+    {
+        var userChoiceAsType = _strChoices[_arg];
+        return new RpsPlayer(userChoiceAsType, _socketCommandContext.User.Username, _socketCommandContext.User.Id);
+    }
+
+    private static GameResult GetGameResult(RpsPlayer first, RpsPlayer second)
+    {
+        var compare = first.Type.Compare(second.Type);
+
+        return compare switch
+        {
+            -1 => new GameResult(first, second, GameResultType.P2),
+            1 => new GameResult(first, second, GameResultType.P1),
+            _ => new GameResult(first, second, GameResultType.Tie)
+        };
+    }
+}

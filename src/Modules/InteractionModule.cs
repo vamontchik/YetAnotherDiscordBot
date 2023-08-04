1186dee6eeeab48db3286953e01846d1c5be9b78
Vlad <vlad.montchik@gmail.com>
2023-08-04 10:39:47 -0600

    add modules

---
+++ src/Modules/InteractionModule.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Discord.Interactions;
+
+namespace DiscordBot.Modules;
+
+public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("ping", "Receive a ping message")]
+    public async Task HandlePingCommand()
+    {
+        await RespondAsync("pong");
+    }
+}
---
+++ src/Modules/PrefixModule.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot.Modules;
+
+public class PrefixModule : ModuleBase<SocketCommandContext>
+{
+    [Command("ping")]
+    public async Task HandlePingCommand()
+    {
+        await Context.Message.ReplyAsync("pong");
+    }
+}

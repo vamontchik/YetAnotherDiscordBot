741e07834509ccf5703b87cbdba5a8fbd2dbea6c
Vlad <vlad.montchik@gmail.com>
2023-08-04 12:33:22 -0600

    add two rps commands: one handles no arg case, and one handles the single arg case

--- src/Modules/PrefixModule.cs
+++ src/Modules/PrefixModule.cs
@@ -1,14 +1,30 @@
 ﻿using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using DiscordBot.Modules.RockPaperScissors;
 
 namespace DiscordBot.Modules;
 
 public class PrefixModule : ModuleBase<SocketCommandContext>
 {
+    public StatsManager StatsManager { get; set; }
+
     [Command("ping")]
     public async Task HandlePingCommand()
     {
         await Context.Message.ReplyAsync("pong");
+    }
+
+    [Command("rps")]
+    public async Task HandleRpsCommandNoArg()
+    {
+        await Context.Message.ReplyAsync("Please specify an argument");
+    }
+
+    [Command("rps")]
+    public async Task HandleRpsCommand(string choice)
+    {
+        var rpsCommand = new RockPaperScissorsCommand(choice, Context, StatsManager);
+        await rpsCommand.ExecuteAsync();
     }
 }

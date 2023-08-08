d0dbd59376f662b1521da118cf0d7286a0a8d6c1
Vlad <vlad.montchik@gmail.com>
2023-08-08 14:40:05 -0600

    refactor to return both a status value and expected result in a tuple, use exception handling as needed, and add CreateFfmpegProcess to this class

--- src/Modules/Audio/MusicFileHandler.cs
+++ src/Modules/Audio/MusicFileHandler.cs
@@ -11,66 +11,83 @@ public static class MusicFileHandler
     private const string FileNameWithoutExtension = "music_file";
     private const string FileNameWithExtension = FileNameWithoutExtension + ".wav";
 
-    public static Task<bool> SafeDeleteMusicAsync(IGuild guild)
+    public static Task<(bool, string)> SafeDeleteMusicAsync(IGuild guild)
     {
         var guildName = guild.Name;
         var guildId = guild.Id;
         var guildIdStr = guildId.ToString();
 
+        AudioLogger.PrintWithGuildInfo(guildName, guildIdStr, "Deleting music file");
+
         try
         {
-            AudioLogger.PrintWithGuildInfo(guildName, guildIdStr, "Deleting music file");
-            var fullPathToDownloadedFile = GetFullPathToDownloadedFile();
-            File.Delete(fullPathToDownloadedFile);
+            File.Delete(GetFullPathToDownloadedFile());
         }
         catch (Exception e)
         {
-            AudioLogger.PrintWithGuildInfo(guildName, guildIdStr, "Unable to delete music file");
             Console.WriteLine(e);
-            return Task.FromResult(false);
+            const string errorMessage = "Unable to delete music file";
+            return Task.FromResult((false, errorMessage));
         }
 
-        return Task.FromResult(true);
+        AudioLogger.PrintWithGuildInfo(guildName, guildIdStr, "Deleted music file");
+
+        return Task.FromResult((true, string.Empty));
     }
 
-    public static async Task<bool> SafeDownloadMusicAsync(IGuild guild, string url)
+    public static async Task<(bool, string)> SafeDownloadMusicAsync(IGuild guild, string url)
     {
         var guildName = guild.Name;
         var guildId = guild.Id;
         var guildIdStr = guildId.ToString();
 
+        AudioLogger.PrintWithGuildInfo(guildName, guildIdStr, "Downloading music file");
+
+        Process nonNullProcess;
         try
         {
-            AudioLogger.PrintWithGuildInfo(guildName, guildIdStr, "Downloading music file");
-            await DownloadMusicFileAsync(url);
+            var process = Process.Start(new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                FileName = "yt-dlp",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                Arguments = $"--extract-audio --audio-format wav {url} -o {FileNameWithoutExtension}"
+            });
+
+            nonNullProcess = process ?? throw new Exception();
         }
         catch (Exception e)
         {
-            AudioLogger.PrintWithGuildInfo(guildName, guildIdStr, "Unable to download music file");
             Console.WriteLine(e);
-            return false;
+            return (false, "Unable to start download process");
         }
 
-        return true;
-    }
-
-    private static async Task DownloadMusicFileAsync(string url)
-    {
-        var startInfo = new ProcessStartInfo
+        try
         {
-            CreateNoWindow = true,
-            UseShellExecute = false,
-            FileName = "yt-dlp",
-            WindowStyle = ProcessWindowStyle.Hidden,
-            Arguments = $"--extract-audio --audio-format wav {url} -o {FileNameWithoutExtension}"
-        };
-        using var process = Process.Start(startInfo);
-        if (process is null)
-            throw new Exception("Unable to create process for ffmpeg");
-        await process.WaitForExitAsync();
+            await nonNullProcess.WaitForExitAsync();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return (false, "Exception occured while waiting for download process to finish");
+        }
+
+        AudioLogger.PrintWithGuildInfo(guildName, guildIdStr, "Downloaded music file");
+
+        return (true, string.Empty);
     }
 
-    public static string GetFullPathToDownloadedFile()
+    public static Process? CreateFfmpegProcess() => Process.Start(new ProcessStartInfo
+    {
+        FileName = "ffmpeg",
+        Arguments =
+            $"-hide_banner -loglevel panic -i \"{GetFullPathToDownloadedFile()}\" -ac 2 -f s16le -ar 48000 pipe:1",
+        UseShellExecute = false,
+        RedirectStandardOutput = true
+    });
+
+    private static string GetFullPathToDownloadedFile()
     {
         var pathToAppContext = Path.GetFullPath(AppContext.BaseDirectory);
         var pathToFile = Path.Combine(pathToAppContext, FileNameWithExtension);

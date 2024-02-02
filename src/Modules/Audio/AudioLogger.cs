9f2247a2a4cbe7569876b44e211524e698c4e89e
Vlad <vlad.montchik@gmail.com>
2024-02-02 14:31:27 -0700

    add logging method for exceptions

--- src/Modules/Audio/AudioLogger.cs
+++ src/Modules/Audio/AudioLogger.cs
@@ -6,10 +6,14 @@ namespace DiscordBot.Modules.Audio;
 public interface IAudioLogger
 {
     void LogWithGuildInfo(IGuild guild, string message);
+    void LogExceptionWithGuildInfo(IGuild guild, Exception e);
 }
 
 public class AudioLogger : IAudioLogger
 {
+    public void LogExceptionWithGuildInfo(IGuild guild, Exception e) =>
+        Console.WriteLine($"[{guild.Name}:{guild.Id}] {e}");
+
     public void LogWithGuildInfo(IGuild guild, string message) =>
         Console.WriteLine($"[{guild.Name}:{guild.Id}] {message}");
 }

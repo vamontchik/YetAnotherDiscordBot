5e9ebf619d8b3e7d352bee9497a7563303b12b02
vlad_montchik <vlad.montchik@gmail.com>
2024-05-28 12:06:33 -0600

    update startup to remove interactions

--- src/Program.cs
+++ src/Program.cs
@@ -45,7 +45,6 @@ internal static class Program
                             LogLevel = LogSeverity.Info,
                             DefaultRunMode = Discord.Interactions.RunMode.Async
                         }))
-                .AddSingleton<InteractionHandler>()
                 .AddSingleton(_ => new CommandService(new CommandServiceConfig
                 {
                     LogLevel = LogSeverity.Info,
@@ -73,14 +72,12 @@ internal static class Program
         using var serviceScope = host.Services.CreateScope();
         var provider = serviceScope.ServiceProvider;
 
-        var interactionService = await InitializeInteractions(provider);
         InitializePrefixCommands(provider);
         var client = provider.GetRequiredService<DiscordSocketClient>();
         var config = provider.GetRequiredService<IConfigurationRoot>();
         var commandService = provider.GetRequiredService<CommandService>();
 
-        SetupClientCallbacks(client, interactionService);
-        SetupInteractionServiceCallbacks(interactionService);
+        SetupClientCallbacks(client);
         SetupCommandServiceCallbacks(commandService);
 
         await client.LoginAsync(TokenType.Bot, config["token"]);
@@ -107,24 +104,7 @@ internal static class Program
         };
     }
 
-    private static void SetupInteractionServiceCallbacks(InteractionService interactionService)
-    {
-        interactionService.Log += message =>
-        {
-            if (message.Exception is CommandException cmdException)
-            {
-                Console.WriteLine($"[Command/{message.Severity}] {cmdException.Command.Aliases[0]}"
-                                  + $" failed to execute in {cmdException.Context.Channel}.");
-                Console.WriteLine(cmdException);
-            }
-            else
-                Console.WriteLine($"[General/{message.Severity}] {message}");
-
-            return Task.CompletedTask;
-        };
-    }
-
-    private static void SetupClientCallbacks(DiscordSocketClient client, InteractionService interactionService)
+    private static void SetupClientCallbacks(DiscordSocketClient client)
     {
         client.Log += message =>
         {
@@ -139,15 +119,12 @@ internal static class Program
 
             return Task.CompletedTask;
         };
-        client.Ready += async () => await interactionService.RegisterCommandsGloballyAsync();
-    }
 
-    private static async Task<InteractionService> InitializeInteractions(IServiceProvider provider)
-    {
-        var interactionService = provider.GetRequiredService<InteractionService>();
-        var interactionHandler = provider.GetRequiredService<InteractionHandler>();
-        await interactionHandler.InitializeAsync();
-        return interactionService;
+        client.Ready += () =>
+        {
+            Console.WriteLine("Ready!");
+            return Task.CompletedTask;
+        };
     }
 
     private static void InitializePrefixCommands(IServiceProvider provider)
